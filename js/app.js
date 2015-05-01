e01b5771ea2d2e1702a7a9933cdaca97714a0f72
Graham Ramsey <g@graham.li>
2015-05-01 12:19:18 -0400

    third section

--- js/app.js
+++ js/app.js
@@ -3,7 +3,12 @@
 $(document).foundation();
 
 $(function(){
+  $("#confirmed").html("CONFIRMED, ID#" + Math.floor((Math.random() * 1000000) + 1));
   $("#confirm").on('click', function(){
     $("#confirmed").fadeIn();
+  });
+
+  $("#generateKey").on('click', function(){
+    $("#key").val((Math.random() + 1).toString(36).substring(10));
   });
 });

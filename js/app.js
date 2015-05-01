a69dd3a20b29c0ecf3e3ad66f84b6cc865888b95
Graham Ramsey <g@graham.li>
2015-05-01 03:54:19 -0400

    parts 1 and 2

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(function(){
+  $("#confirm").on('click', function(){
+    $("#confirmed").fadeIn();
+  });
+});
